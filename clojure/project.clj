563d0a729513390a909b1447b893ef5a758ab832
Iván Perdomo <ivan@perdomo.me>
2013-10-19 17:37:44 +0200

    Build a simple page that contains the image

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,13 +1,14 @@
-(defproject me.perdomo.cofre "0.1.0"
+(defproject me.perdomo.cofre "0.2.0"
   :description "Small HTTP server to process image upload/get requests"
   :url "http://github.com/iperdomo/cofre"
-  :license {:name "Apache Public License"
+  :license {:name "Apache Public License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring/ring-core "1.2.0"]
                  [ring/ring-jetty-adapter "1.2.0"]
-                 [compojure "1.1.5"]]
+                 [compojure "1.1.5"]
+                 [hiccup "1.0.4"]]
 
   :main me.perdomo.cofre.http
   :aot [me.perdomo.cofre.http])
--- clojure/src/me/perdomo/cofre/http.clj
+++ clojure/src/me/perdomo/cofre/http.clj
@@ -4,7 +4,9 @@
             [ring.middleware.multipart-params :refer (wrap-multipart-params)]
             [ring.adapter.jetty :refer (run-jetty)]
             [ring.util.response :refer (response content-type not-found file-response)]
-            [compojure.core :refer (defroutes GET POST)])
+            [compojure.core :refer (defroutes GET POST)]
+            [compojure.route :refer (resources)]
+            [hiccup.page :refer (html5)])
   (:gen-class))
 
 (defn- get-file-name []
@@ -30,9 +32,24 @@
       (-> (file-response (.getAbsolutePath image))
         (content-type "image/jpeg")))))
 
+(defn get-html [id]
+  (html5
+    [:head
+      [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
+      [:meta {:http-equiv "Content-Type" :content "text/html; charset=utf8"}]
+      [:meta {:name "apple-mobile-web-app-capable" :content "yes"}]
+      [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0, maximum-scale=1.0"}]
+      [:link {:rel "stylesheet" :href "public/pure-min.css"}]
+      [:title ""]]
+    [:body
+     [:div {:style "width: 100%; height: 100%; position: absolute; left: 0px; top: 0px; z-index: 0;"}
+      [:img {:src (str "img/" id) :style "min-width:100%; min-height:100%; width:auto; height:auto;"}]]]))
+
 (defroutes routes
   (GET "/" req "Hi there!")
-  (GET "/:id" [id] (get-image id))
+  (GET "/:id" [id] (get-html id))
+  (GET "/img/:id" [id] (get-image id))
+  (resources "/public")
   (POST "/" req (upload req)))
 
 (def app
